a852f183068f1ef67dd8e73241a6f9a6c9b4d244
MGibadulin <gibadulin.m.a@gmail.com>
2023-04-07 00:21:52 +0500

    Update

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+"""
+A script extracts and processes data from a specific csv file with
+data from a car listing website.
+It enters the result into the standard output window.
+"""
+
 import argparse
+from pathlib import Path
+
 
 def get_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="A script extracts and processes data frome \
-                                 a specific csv file with data from a car listing website")
+    """Get args"""
+
+    parser = argparse.ArgumentParser(
+        description="A script extracts and processes data from \
+                                 a specific csv file with data from a car listing website"
+    )
 
     parser.add_argument("--brand", type=str, help="Vehicle manufacturer")
     parser.add_argument("--year_from", type=int, help="Build date vehicle from")
@@ -13,20 +25,34 @@ def get_args() -> argparse.Namespace:
     parser.add_argument("--transmission", type=str, help="Type of transmission")
     parser.add_argument("--mileage", type=int, help="Maximal mileage")
     parser.add_argument("--body", type=str, help="Type of body vehicle")
-    parser.add_argument("--engine_from", type=int, help="Minimal volume of engine in cm^3")
-    parser.add_argument("--engine_to", type=int, help="Maximal volume of engine in cm^3")
+    parser.add_argument(
+        "--engine_from", type=int, help="Minimal volume of engine in cm^3"
+    )
+    parser.add_argument(
+        "--engine_to", type=int, help="Maximal volume of engine in cm^3"
+    )
     parser.add_argument("--fuel", type=str, help="Type of fuel")
     parser.add_argument("--exchange", type=str, help="Ready to exchange, yes/no")
-    parser.add_argument("--keywords", 
-                    type=str, 
-                    help="Any text you are looking for in the ad, independent keywords separated by commas"
-                    )
-    parser.add_argument("--max_records", type=int, default=20, help="Maximal number of records output")
+    parser.add_argument(
+        "--keywords",
+        type=str,
+        help="Any text you are looking for in the ad,\
+                    independent keywords separated by commas",
+    )
+    parser.add_argument(
+        "--max_records", type=int, default=20, help="Maximal number of records output"
+    )
 
     args = parser.parse_args()
     return args
 
-if __name__ == "__main__":
-    args = get_args()
 
-    print(type(args))
+def load_data(file_path: Path) -> Path:
+    """Load data from CSV file"""
+    return file_path
+
+
+if __name__ == "__main__":
+    args_app = get_args()
+
+    print(type(args_app))
