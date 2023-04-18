c7401a52909bdf7198340ef138e9707815490c71
MGibadulin <gibadulin.m.a@gmail.com>
2023-04-18 21:38:53 +0500

    Rename files

--- carsetl.py
+++ carsetl.py
@@ -4,10 +4,8 @@ data from a car listing website.
 It enters the result into the standard output window.
 """
 
-import argparse
 import csv
 import io
-from datetime import datetime
 from pathlib import Path
 
 from tabulate import tabulate
@@ -27,57 +25,6 @@ EXCHANGE_MAP = {
     "Возможен обмен с вашей доплатой": "yes",
     "Обмен не интересует": "no",
 }
-
-
-def get_args() -> dict:
-    """Get args"""
-
-    parser = argparse.ArgumentParser(
-        description="A script extracts and processes data from \
-                                 a specific csv file with data from a car listing website"
-    )
-
-    parser.add_argument("-brand", type=str, default=None, help="Vehicle manufacturer")
-    parser.add_argument(
-        "-year_from", type=int, default=0, help="Build date vehicle from"
-    )
-    parser.add_argument(
-        "-year_to", type=int, default=2099, help="Build date vehicle to"
-    )
-    parser.add_argument("-model", type=str, default=None, help="Model vehicle")
-    parser.add_argument("-price_from", type=int, default=0, help="Minimal price in USD")
-    parser.add_argument(
-        "-price_to", type=int, default=10**9, help="Maximal price in USD"
-    )
-    parser.add_argument("-transmission", type=str, help="Type of transmission")
-    parser.add_argument("-mileage", type=int, default=10**6, help="Maximal mileage")
-    parser.add_argument("-body", type=str, help="Type of body vehicle")
-    parser.add_argument(
-        "-engine_from", type=int, default=0, help="Minimal volume of engine in cm^3"
-    )
-    parser.add_argument(
-        "-engine_to", type=int, default=10000, help="Maximal volume of engine in cm^3"
-    )
-    parser.add_argument("-fuel", type=str, help="Type of fuel")
-    parser.add_argument("-exchange", type=str, help="Ready to exchange, yes/no")
-    parser.add_argument(
-        "-keywords",
-        type=str,
-        default="",
-        help="Any text you are looking for in the ad,\
-                    independent keywords separated by commas",
-    )
-    parser.add_argument(
-        "-max_records", type=int, default=20, help="Maximal number of records output"
-    )
-    parser.add_argument(
-        "-file",
-        type=Path,
-        default="data/cars-av-by_card_v2.csv",
-        help="Path to file with data",
-    )
-    args = parser.parse_args()
-    return vars(args)
 
 
 def extract_brand(input_string: str) -> str:
@@ -330,7 +277,7 @@ FILTERS_PIPELINE = (
 )
 
 
-def filter_data(input_data: list, param_filters: argparse.Namespace) -> list:
+def filter_data(input_data: list, param_filters: dict) -> list:
     """Filtering rows by parameters"""
     filtered_data = []
     for row in input_data:
@@ -357,38 +304,3 @@ def order_data(input_data: list):
 def load_out_data(input_data: list, max_records):
     """Print date in table format"""
     print(tabulate(input_data[:max_records], headers="keys"))
-
-
-def main():
-    """Main function"""
-    start_time = datetime.now()
-
-    args_app = get_args()
-    ts_args_app = datetime.now()
-
-    input_data = load_data(args_app["file"])
-    ts_load = datetime.now()
-
-    extracted_data = extract_data(input_data)
-    ts_extract = datetime.now()
-
-    filtered_data = filter_data(extracted_data, args_app)
-    ts_filter = datetime.now()
-
-    order_data(filtered_data)
-    ts_order = datetime.now()
-
-    load_out_data(filtered_data, args_app["max_records"])
-    ts_show = datetime.now()
-
-    print(f"Parse args: {ts_args_app - start_time}")
-    print(f"Load: {ts_load - ts_args_app}")
-    print(f"Extract: {ts_extract - ts_load}")
-    print(f"Filter: {ts_filter - ts_extract}")
-    print(f"Order: {ts_order - ts_filter}")
-    print(f"Show: {ts_show - ts_order}")
-    print(f"Total: {ts_show - start_time}")
-
-
-if __name__ == "__main__":
-    main()
