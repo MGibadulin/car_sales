ccb52bef2d42a12884a2ec8ae9bb3398b306d117
MGibadulin <gibadulin.m.a@gmail.com>
2023-04-20 15:29:21 +0200

    Add logging

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import carsetl as etl
 import logging
 import time
-
+import os
 
 def get_args() -> dict:
     """Get args"""
@@ -62,24 +62,40 @@ def main():
 
     logging.basicConfig(
         level=logging.INFO,
-        filename="myProgramLog.txt",
+        filename="log.log",
         encoding="utf-8",
         format=" %(asctime)s - %(levelname)s - %(message)s",
     )
 
     start_time = time.perf_counter()
+    
     args_app = get_args()
     end_time_args = time.perf_counter()
-
+    logging.info(f"Time elapsed get_args(): {(end_time_args - start_time):.3f} sec")
+    
     input_data = etl.load_data(args_app["file"])
+    end_time_load = time.perf_counter()
+    logging.info(f"Time elapsed load_data(): {(end_time_load - end_time_args):.3f} sec")
+    
+    file_stats = os.stat(args_app["file"])
+    logging.info(f"File Size is {(file_stats.st_size / (1024 * 1024)):.2f} Mb")
+    
 
     extracted_data = etl.extract_data(input_data)
-
+    end_time_extract = time.perf_counter()
+    logging.info(f"Time elapsed extract_data(): {(end_time_extract - end_time_load):.3f} sec")
+  
     filtered_data = etl.filter_data(extracted_data, args_app)
+    end_time_filter = time.perf_counter()
+    logging.info(f"Time elapsed filter_data(): {(end_time_filter - end_time_extract):.3f} sec")
 
     etl.order_data(filtered_data)
+    end_time_order = time.perf_counter()
+    logging.info(f"Time elapsed order_data(): {(end_time_order - end_time_filter):.3f} sec")
 
     etl.load_out_data(filtered_data, args_app["max_records"])
+    end_time_out = time.perf_counter()
+    logging.info(f"Time elapsed load_out_data(): {(end_time_out - end_time_order):.3f} sec")
 
 
 if __name__ == "__main__":
